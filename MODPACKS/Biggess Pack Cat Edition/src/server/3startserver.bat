0f23d8fb976f2b017bb701cdfcbc1fa67859199e
quentin452 <42176772+quentin452@users.noreply.github.com>
2025-10-01 09:17:32 +0200

    Biggess Pack Cat Edition V1 Version 1.0.7

--- MODPACKS/Biggess Pack Cat Edition/src/server/3startserver.bat
+++ MODPACKS/Biggess Pack Cat Edition/src/server/3startserver.bat
@@ -1,6 +1,6 @@
 :: Lancez votre serveur Minecraft
 echo Démarrage du serveur Minecraft...
-"C:\YOUR\JAVA21+\PATH.exe" -Xmx7G -Xms500M -Xmx7G -Xms500M -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+UseStringDeduplication -XX:SoftRefLRUPolicyMSPerMB=50 -XX:+UseCompressedOops -XX:+TieredCompilation -XX:+OptimizeStringConcat @java9args.txt -jar lwjgl3ify-2.1.15-forgePatches.jar nogui
+"C:\YOUR\JAVA21+\PATH.exe" -Xmx7G -Xms500M -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:+ParallelRefProcEnabled -XX:+UseCompressedOops -XX:+TieredCompilation -XX:+OptimizeStringConcat -XX:SoftRefLRUPolicyMSPerMB=100 @java9args.txt -jar lwjgl3ify-2.1.15-forgePatches.jar nogui
 
 :: Fin du script
 pause
